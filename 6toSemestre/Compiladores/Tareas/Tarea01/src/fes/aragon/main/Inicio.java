ad7a490e03eda2338d4fc1d3ba61db8d3655aa5c
Acxir8 <henrry.mariaca.vaz@gmail.com>
2024-02-01 19:16:25 -0600

    Se agregaran todos los documentos del nuevo semestre

---
+++ 6toSemestre/Compiladores/Tareas/Tarea01/src/fes/aragon/main/Inicio.java
@@ -0,0 +1,21 @@
+package fes.aragon.main;
+
+import fes.aragon.metodos.*;
+
+public class Inicio {
+	public static void main(String[] args) {
+		Archivos archivos = new Archivos();
+		String nombreArchivo = System.getProperty("user.dir") + "\\src\\fes\\aragon\\recursos\\marianela.fes";
+		String nombreArchivoEscritura = "Escrito.fes";
+
+//		//Lecturas
+		archivos.leerArchivo(nombreArchivo);
+		archivos.imprimirArchivo();
+
+		// Escritura
+		archivos.guardarArchivo(nombreArchivoEscritura);
+		archivos.leerArchivo(System.getProperty("user.dir") + "\\src\\fes\\aragon\\recursos\\" + nombreArchivoEscritura);
+		archivos.imprimirArchivo();
+
+	}
+}
